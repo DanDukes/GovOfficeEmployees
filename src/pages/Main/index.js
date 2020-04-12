fb9e87d23296d73237a80dc51f7243b91d283b15
Dan D <degeare28@gmail.com>
2020-04-12 14:03:23 -0600

    set up states and implemented a field filter

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
 import SearchBox from "../../components/SearchBox";
 import Table from "../../components/Table";
 import Employees from "../../utils/Employee.json";
 
 export default function Main() {
+    const [search, setSearch] = useState("");
+    const [sortedField, setSortedField] = useState("");
+
+    setSearch("Cox");
+
+    //Filter method from https://scotch.io/tutorials/quick-and-simple-search-filter-using-vanilla-javascript
+    let filteredName = Employees.filter(target => target["Employee_Name"].toLowerCase().includes(search.toLowerCase()));
+
+    console.log(filteredName);
+
     return (
         <div>
         <Navbar />
