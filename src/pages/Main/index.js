e127003146aafa62fcb7844b3784565cbfd794c1
Dan D <degeare28@gmail.com>
2020-04-12 13:39:21 -0600

    set up functional components and import them

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from "../../components/Navbar";
+import SearchBox from "../../components/SearchBox";
+import Table from "../../components/Table";
 
 export default function Main() {
     return (
         <div>
-            
+        <Navbar />
+        <SearchBox />
+        <Table/> 
         </div>
     )
 }
