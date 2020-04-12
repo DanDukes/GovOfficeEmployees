69274bc5aecda4764e8a584299194fe7c1890db2
Dan D <degeare28@gmail.com>
2020-04-12 13:42:25 -0600

    import json data into the app

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from "../../components/Navbar";
 import SearchBox from "../../components/SearchBox";
 import Table from "../../components/Table";
+import Employees from "../../utils/Employee.json";
 
 export default function Main() {
     return (
