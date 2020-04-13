47f003558ca8571806dc1eaa5da50968b07e7c48
Dan D <degeare28@gmail.com>
2020-04-12 21:40:33 -0600

    Updates

---
+++ precache-manifest.5a786e00b1ea24f629a37302df784811.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da04a87ebe80e9d13d33e96a770a47b1",
+    "url": "/GovOfficeEmployees/index.html"
+  },
+  {
+    "revision": "03d93c1b504add67f36b",
+    "url": "/GovOfficeEmployees/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c612cdad1ab42ed8b8be",
+    "url": "/GovOfficeEmployees/static/js/2.87e124f6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/GovOfficeEmployees/static/js/2.87e124f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d93c1b504add67f36b",
+    "url": "/GovOfficeEmployees/static/js/main.2fbb33a9.chunk.js"
+  },
+  {
+    "revision": "0b7424b07786ddbd3e79",
+    "url": "/GovOfficeEmployees/static/js/runtime-main.3779dab8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dandukes.github.io/GovOfficeEmployees/precache-manifest.6d63e85a0886a952b99c9782f92d2405.js"
+  "/GovOfficeEmployees/precache-manifest.5a786e00b1ea24f629a37302df784811.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dandukes.github.io/GovOfficeEmployees/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GovOfficeEmployees/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
